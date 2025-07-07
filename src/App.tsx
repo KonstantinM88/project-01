b893214dff59855e048abf1a1731bdf2a1faa831
KonsnantinM <mihajlov8@gmail.com>
2025-07-07 23:41:32 +0200

    img3

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ return (
       />
     <ProfileCard
       //  avatar="https://u.to/2r9OIg "
-        avatar='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEw9tV94lQzazcryxEpr8lIdv7t7Unc0roQcJLNub3ud_60sXZOwJqnv8&s'
+        avatar="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEw9tV94lQzazcryxEpr8lIdv7t7Unc0roQcJLNub3ud_60sXZOwJqnv8&s"
         name="Jane Smith"
         description="A creative designer."
       />
