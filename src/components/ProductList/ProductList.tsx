1f276fbccfd785e1bee52f362f20aa68f0e35e3e
KonsnantinM <mihajlov8@gmail.com>
2025-07-15 02:40:24 +0200

    hw_07style1

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import type { Product } from "../../types/types";
 import { Link } from "react-router-dom";
+import styles from "./ProductList.module.css";
 
 export const ProductsList = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -17,14 +18,19 @@ export const ProductsList = () => {
   }
 
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Products list</h2>
-      <ul>
+      <ul className={styles.list}>
         {products.map((p) => (
-          <li key={"product" + p.id}>
-            <h3>{p.title}</h3>
-            <img src={p.images[0]} alt="product" />
-            <span>{p.price} $</span>
+          // <li key={"product" + p.id}>
+          //   <h3>{p.title}</h3>
+          //   <img src={p.images[0]} alt="product" />
+          //   <span>{p.price} $</span>
+          <li key={"product" + p.id} className={styles.item}>
+          <h3 className={styles.title}>{p.title}</h3>
+          <img src={p.images[0]} alt="product" className={styles.image} />
+          <p className={styles.description}>{p.description}</p>
+          <span className={styles.price}>{p.price} $</span>
             <Link to={`/products/${p.id}`}>To product</Link>
           </li>
         ))}
--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import useUsers from "../../hooks/useUsers";
+import styles from "./UsersList.module.css";
 
 export const UsersList = () => {
   // custom hook
@@ -14,15 +15,17 @@ export const UsersList = () => {
   }
 
   return (
-    <div>
-      <ul>
+    <div className={styles.usersList}>
+      <ul className={styles.list}>
         {users.map((u) => (
-          <li key={u.id}>
-            <Link to={`/users/${u.id}`}>{u.name}</Link>
+          // <li key={u.id}>
+          //   <Link to={`/users/${u.id}`}>{u.name}</Link>
+          <li key={u.id} className={styles.userItem}>
             <img
               src={u.avatar}
               alt="avatar"
-              width={"200px"}
+              // width={"200px"}
+              className={styles.avatar}
               onError={(e) => {
                 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                 // @ts-ignore
@@ -30,6 +33,10 @@ export const UsersList = () => {
                   "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small_2x/default-avatar-photo-placeholder-profile-icon-vector.jpg";
               }}
             />
+              <Link to={`/users/${u.id}`} className={styles.name}>
+              {u.name}
+            </Link>
+            <p className={styles.email}>{u.email}</p>
           </li>
         ))}
       </ul>
